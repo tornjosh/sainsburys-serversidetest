ae178d5c2ec6e5810239c3de31b8121e012e43e2
Joshua Torn <josh.torn@btopenworld.com>
2018-05-21 14:26:44 +0100

    Add scrapeTitle method to GrocerisScraper

--- src/main/java/com/josh/torn/sainsburys/GroceriesScraper.java
+++ src/main/java/com/josh/torn/sainsburys/GroceriesScraper.java
@@ -23,11 +23,38 @@ public class GroceriesScraper {
         // finds all product elements
         Elements products = doc.select("div.product");
 
+        // array of the products of the page
+        ProductItem[] items = new ProductItem[products.size()];
+
+        int index = 0;
+
+        // loops through element selecting one element at a time and scraps information
         for(Element product: products) {
+
+            String title = scrapeTitle(product);
+
+            double unitPrice = 0;
+
+            int kcalPer100g = 0;
+
+            String description = "";
+
+            ProductItem item = new ProductItem(title, unitPrice, kcalPer100g, description);
+
+            items[index] = item;
+
+            System.out.println(items[index].getTitle());
+
+            index++;
         }
 
         // prints the html
-        System.out.println(products.html());
+        //System.out.println(products.html());
 
+    }
+
+    // scrapes the title of a product from the product element.
+    private static String scrapeTitle(Element element) {
+        return element.select("a").first().text();
     }
 }
--- src/test/java/com/josh/torn/sainsburys/GroceriesScraperTest.java
+++ src/test/java/com/josh/torn/sainsburys/GroceriesScraperTest.java
@@ -2,8 +2,6 @@ package com.josh.torn.sainsburys;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class GroceriesScraperTest {
 
 }
